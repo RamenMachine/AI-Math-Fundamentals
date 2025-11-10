02622ad8f2e004634b9556a4a509abed50e7612a
RamenMachine <emailameenrahman@gmail.com>
2025-11-09 19:19:27 -0600

    Clean HW3: Keep only pweek3.py for submission

--- HW3/read_pdf.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-with open('python-week3(1).pdf', 'rb') as pdfFile:
-    pdfReader = PyPDF2.PdfReader(pdfFile)
-    
-    for pageNum in range(len(pdfReader.pages)):
-        page = pdfReader.pages[pageNum]
-        print(f"\n=== PAGE {pageNum + 1} ===\n")
-        print(page.extract_text())
